d76c3e2aea767f63f375511a9ebc1c9f46ffcb33
andre <andre@AndreasHMS>
2025-04-07 13:55:29 -0300

    exercicio7

---
+++ src/main/java/com/mycompany/exercicio7/Exercicio7.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.exercicio7;
+
+import javax.swing.JOptionPane;
+public class Exercicio7 {
+
+    public static void main(String[] args) {
+        String nome;
+        nome = JOptionPane.showInputDialog("Digite seu Nome:");
+        JOptionPane.showMessageDialog(null, "Nome:" + nome);
+    }
+}
